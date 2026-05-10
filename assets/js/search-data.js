b7f9f5bf33af842d311f4cddc57091990c9426e8
yw4291 <160531067+yw4291@users.noreply.github.com>
2026-05-10 20:08:40 +0000

    Deploying to gh-pages from @ yw4291/yw4291.github.io@81c2cabffa24f62c09a1abfb7d5f03d8e2956206 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -535,13 +535,6 @@ ninja.data = [{
           window.open("https://scholar.google.com/citations?user=_bWU9yUAAAAJ", "_blank");
         },
       },{
-        id: 'social-custom_social',
-        title: 'Custom_social',
-        section: 'Socials',
-        handler: () => {
-          window.open("https://www.alberteinstein.com/", "_blank");
-        },
-      },{
       id: 'light-theme',
       title: 'Change theme to light',
       description: 'Change the theme of the site to Light',
