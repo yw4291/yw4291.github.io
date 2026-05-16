91ea765834a74890516c002c64f91933daf9dc7a
yw4291 <160531067+yw4291@users.noreply.github.com>
2026-05-16 16:25:50 +0000

    Deploying to gh-pages from @ yw4291/yw4291.github.io@0acd8d1ba17268953e512e0f4759298f42a4ecfd 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -467,13 +467,6 @@ ninja.data = [{
           window.open("mailto:%77%61%6E%67%79%65%62%69%6F%69%6E%66%6F%32@%67%6D%61%69%6C.%63%6F%6D", "_blank");
         },
       },{
-        id: 'social-github',
-        title: 'GitHub',
-        section: 'Socials',
-        handler: () => {
-          window.open("https://github.com/", "_blank");
-        },
-      },{
         id: 'social-linkedin',
         title: 'LinkedIn',
         section: 'Socials',
