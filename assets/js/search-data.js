0d370c88ed190b32c422813d86a21a6e7eb38e4e
yw4291 <160531067+yw4291@users.noreply.github.com>
2026-05-16 15:23:26 +0000

    Deploying to gh-pages from @ yw4291/yw4291.github.io@c6984258a53d43e527d8d990867188e88420a0cc 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -440,10 +440,7 @@ ninja.data = [{
           description: "",
           section: "Books",handler: () => {
               window.location.href = "/books/the_godfather/";
-            },},{id: "news-a-simple-inline-announcement-with-markdown-emoji-sparkles-smile",
-          title: 'A simple inline announcement with Markdown emoji! :sparkles: :smile:',
-          description: "",
-          section: "News",},{id: "projects-project-1",
+            },},{id: "projects-project-1",
           title: 'project 1',
           description: "with background image",
           section: "Projects",handler: () => {
