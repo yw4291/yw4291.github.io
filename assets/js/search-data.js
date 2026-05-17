b0af8af3e3c2677fde1938e0d67246ddf13cc8b3
yw4291 <160531067+yw4291@users.noreply.github.com>
2026-05-17 13:11:39 +0000

    Deploying to gh-pages from @ yw4291/yw4291.github.io@207e7e6a40a75bbf31b1e3828b7e4e045cd3f454 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -16,6 +16,13 @@ ninja.data = [{
           handler: () => {
             window.location.href = "/publications/";
           },
+        },{id: "nav-cv",
+          title: "cv",
+          description: "",
+          section: "Navigation",
+          handler: () => {
+            window.location.href = "/cv/";
+          },
         },{
         id: 'social-email',
         title: 'email',
